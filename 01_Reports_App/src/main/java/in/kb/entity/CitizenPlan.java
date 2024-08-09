ade872c8a62a23c62657213499075e374779a337
kbaduwal <khakendra1999@gmail.com>
2024-08-09 23:24:40 +0545

    Entity and Repository work is completed.

--- 01_Reports_App/src/main/java/in/kb/entity/CitizenPlan.java
+++ 01_Reports_App/src/main/java/in/kb/entity/CitizenPlan.java
@@ -1,2 +1,25 @@
-package in.kb.entity;public class CitizenPlan {
+package in.kb.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "CITIZEN_PLAN_INFO")
+public class CitizenPlan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer citizenId;
+    private String citizenName;
+    private String gender;
+    private String planName;
+    private String planStatus;
+    private LocalDate planStartDate;
+    private LocalDate planEndDate;
+    private Double benefitAmount;
+    private String denialReason;
+    private LocalDate terminatedDate;
+    private String terminatedReason;
 }
--- 01_Reports_App/src/main/java/in/kb/repo/CitizenPlanRepository.java
+++ 01_Reports_App/src/main/java/in/kb/repo/CitizenPlanRepository.java
@@ -1,2 +1,17 @@
-package in.kb.repo;public interface CitizenPlanRepository {
+package in.kb.repo;
+
+import in.kb.entity.CitizenPlan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface CitizenPlanRepository extends JpaRepository<CitizenPlan, Integer> {
+
+    @Query("SELECT DISTINCT(planName) FROM CitizenPlan")
+    public List<String> getPlanNames();
+
+    @Query("SELECT DISTINCT(planStatus) FROM CitizenPlan")
+    public List<String> getPlanStatus();
+
 }
