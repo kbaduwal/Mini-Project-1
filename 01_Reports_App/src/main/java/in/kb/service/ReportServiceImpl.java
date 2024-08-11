ddc2c634fd7b0c04d3a546fab1cfa773f2ebdc43
kbaduwal <khakendra1999@gmail.com>
2024-08-11 23:59:15 +0530

    Added the code for dynamics search field and date picker.

--- 01_Reports_App/src/main/java/in/kb/service/ReportServiceImpl.java
+++ 01_Reports_App/src/main/java/in/kb/service/ReportServiceImpl.java
@@ -3,9 +3,13 @@ package in.kb.service;
 import in.kb.entity.CitizenPlan;
 import in.kb.repo.CitizenPlanRepository;
 import in.kb.request.SearchRequest;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Service
@@ -30,9 +34,41 @@ public class ReportServiceImpl implements ReportService {
 
     @Override
     public List<CitizenPlan> search(SearchRequest request) {
-        List<CitizenPlan> searchResult = planRepo.findAll();
-        System.out.println("\n Search is happening some issue with JSP \n"+searchResult);
-        return planRepo.findAll();
+        CitizenPlan entity = new CitizenPlan();
+
+        if(null!=request.getPlanName() && !"".equals(request.getPlanName())){
+            entity.setPlanName(request.getPlanName());
+        }
+
+        if(null!=request.getPlanStatus() && !"".equals(request.getPlanStatus())) {
+            entity.setPlanStatus(request.getPlanStatus());
+        }
+
+        if(null!=request.getGender() && !"".equals(request.getGender())){
+            entity.setGender(request.getGender());
+        }
+
+        if(null!=request.getStartDate() && !"".equals(request.getStartDate())){
+          String  startDate= request.getStartDate();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+            //convert String to LocalDate
+            LocalDate localDate = LocalDate.parse(startDate, formatter);
+            entity.setPlanStartDate(localDate);
+
+        }
+
+        if(null!=request.getEndDate() && !"".equals(request.getEndDate())){
+            String  endDate= request.getEndDate();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+            //convert String to LocalDate
+            LocalDate localDate = LocalDate.parse(endDate, formatter);
+            entity.setPlanEndDate(localDate);
+
+        }
+
+        return planRepo.findAll(Example.of(entity));
     }
 
     @Override
